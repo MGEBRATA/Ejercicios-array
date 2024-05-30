dcc99fd1cd7d36bd1934383c2afddc0fc653ac90
WATIL <manupebe7@gmail.com>
2024-05-30 15:51:07 -0300

    3

---
+++ Assets/Scripts/AlquilerVehiculos.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlquilerVehiculos : MonoBehaviour
+{
+    public GameObject[] vehiculos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OcultarVehiculos();
+        vehiculos[0].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            MostrarVehiculos();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            OcultarVehiculos();
+            int f = Random.Range(0, vehiculos.Length);
+            vehiculos[f].SetActive(true);
+        }
+    }
+
+    void OcultarVehiculos()
+    {
+        for(int i = 0; i < vehiculos.Length; i++)
+        {
+            vehiculos[i].SetActive(false);
+        }
+    }
+
+    void MostrarVehiculos()
+    {
+        for (int i = 0; i < vehiculos.Length; i++)
+        {
+            vehiculos[i].SetActive(true);
+        }
+    }
+}
